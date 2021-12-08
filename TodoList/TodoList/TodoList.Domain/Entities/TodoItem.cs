26b09c050ebb4d5c42a68968b378eef3eea27b36
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-08 15:57:23 +0300

    Domain model layout

---
+++ TodoList/TodoList/TodoList.Domain/Entities/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TodoList.Domain.Enums;
+
+namespace TodoList.Domain.Entities
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public Guid ListId { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public Priority PriorityLevel { get; set; }
+        public TodoList List { get; set; }
+    }
+}
---
+++ TodoList/TodoList/TodoList.Domain/Entities/TodoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoList.Domain.Entities
+{
+    public class TodoList
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime LastEditDate { get; set; }
+        public IList<TodoItem> Items { get; set; }
+    }
+}
