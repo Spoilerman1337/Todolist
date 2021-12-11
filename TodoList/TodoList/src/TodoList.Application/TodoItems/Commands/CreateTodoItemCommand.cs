e8d02b06ec6228d7046129059d8f678e4b73bd8a
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-11 13:18:15 +0300

    create TodoItem commands

---
+++ TodoList/TodoList/src/TodoList.Application/TodoItems/Commands/CreateTodoItemCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+
+namespace TodoList.Application.TodoItems.Commands
+{
+    public class CreateTodoItemCommand : IRequest<Guid>
+    {
+        public Guid ListId { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ TodoList/TodoList/src/TodoList.Application/TodoItems/Commands/CreateTodoItemCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TodoList.Application.Common.Interfaces;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Application.TodoItems.Commands
+{
+    public class CreateTodoItemCommandHandler : IRequestHandler<CreateTodoItemCommand, Guid>
+    {
+        private readonly ITodoListDbContext _context;
+
+        public CreateTodoItemCommandHandler(ITodoListDbContext dbContext) => _context = dbContext;
+
+        public async Task<Guid> Handle(CreateTodoItemCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new TodoItem
+            {
+                ListId = request.ListId,
+                Title = request.Title,
+                IsDone = false,
+                Id = Guid.NewGuid()
+            };
+
+            _context.TodoItems.Add(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
