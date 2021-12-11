2b0077d15d72cb40c1c422e0d418250759d004bf
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-11 20:16:30 +0300

    create todoitemlist command

---
+++ TodoList/TodoList/src/TodoList.Application/TodoItemsLists/Commands/CreateTodoItemsList/CreateTodoItemsListCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+
+namespace TodoList.Application.TodoItemsLists.Commands.CreateTodoItemsList
+{
+    public class CreateTodoItemsListCommand : IRequest<Guid>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ TodoList/TodoList/src/TodoList.Application/TodoItemsLists/Commands/CreateTodoItemsList/CreateTodoItemsListCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TodoList.Application.Common.Interfaces;
+using TodoList.Domain.Entities;
+
+namespace TodoList.Application.TodoItemsLists.Commands.CreateTodoItemsList
+{
+    public class CreateTodoItemsListCommandHandler : IRequestHandler<CreateTodoItemsListCommand, Guid>
+    {
+        private readonly ITodoListDbContext _context;
+
+        public CreateTodoItemsListCommandHandler(ITodoListDbContext dbContext) => _context = dbContext;
+
+        public async Task<Guid> Handle(CreateTodoItemsListCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new TodoItemsList
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Description = request.Description,
+                LastEditDate = DateTime.Now
+            };
+
+            _context.TodoLists.Add(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
