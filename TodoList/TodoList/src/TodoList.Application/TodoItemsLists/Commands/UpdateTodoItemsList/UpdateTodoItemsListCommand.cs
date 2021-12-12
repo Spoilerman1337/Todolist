f321ce42194c96f00d1aaad5c979f181cd9d3daf
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-12 16:31:20 +0300

    update todo list command

---
+++ TodoList/TodoList/src/TodoList.Application/TodoItemsLists/Commands/UpdateTodoItemsList/UpdateTodoItemsListCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+
+namespace TodoList.Application.TodoItemsLists.Commands.UpdateTodoItemsList
+{
+    public class UpdateTodoItemsListCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ TodoList/TodoList/src/TodoList.Application/TodoItemsLists/Commands/UpdateTodoItemsList/UpdateTodoItemsListCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using TodoList.Application.Common.Exceptions;
+using TodoList.Application.Common.Interfaces;
+
+namespace TodoList.Application.TodoItemsLists.Commands.UpdateTodoItemsList
+{
+    public class UpdateTodoItemsListCommandHandler : IRequestHandler<UpdateTodoItemsListCommand>
+    {
+        private readonly ITodoListDbContext _context;
+
+        public UpdateTodoItemsListCommandHandler(ITodoListDbContext dbContext) => _context = dbContext;
+
+        public async Task<Unit> Handle(UpdateTodoItemsListCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.TodoLists.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(TodoList), request.Id);
+            }
+
+            entity.Name = request.Name;
+            entity.Description = request.Description;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
