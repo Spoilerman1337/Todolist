46066f8bc7e947e19b8632504a589b1c7c60a4ee
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-19 16:22:56 +0300

    added application DI

---
+++ TodoList/TodoList/src/TodoList.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Reflection;
+
+namespace TodoList.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AppApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
