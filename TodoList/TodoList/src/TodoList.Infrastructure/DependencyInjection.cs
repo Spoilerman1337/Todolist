95dd5072819509f77e028094fc1ceedefbdfe399
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-19 16:15:54 +0300

    added infrastructure DI

---
+++ TodoList/TodoList/src/TodoList.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoList.Application.Common.Interfaces;
+using TodoList.Infrastructure.Persistence;
+
+namespace TodoList.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TodoListDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ITodoListDbContext>(provider =>
+                provider.GetService<TodoListDbContext>());
+
+            return services;
+        }
+    }
+}
