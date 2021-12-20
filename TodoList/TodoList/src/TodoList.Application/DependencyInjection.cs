ef0b850cb2945f8e6baa0b09b89785c0779bbfb4
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-20 17:07:32 +0300

    added automapper DI in application DI registered services in startup.cs

--- TodoList/TodoList/src/TodoList.Application/DependencyInjection.cs
+++ TodoList/TodoList/src/TodoList.Application/DependencyInjection.cs
@@ -6,9 +6,10 @@ namespace TodoList.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AppApplication(this IServiceCollection services)
+        public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
             return services;
         }
--- TodoList/TodoList/src/TodoList.Presentation/Startup.cs
+++ TodoList/TodoList/src/TodoList.Presentation/Startup.cs
@@ -1,23 +1,26 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Reflection;
-using TodoList.Application.Common.Interfaces;
-using TodoList.Application.Common.Mappings;
+using TodoList.Application;
+using TodoList.Infrastructure;
 
 namespace TodoList.Presentation
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAutoMapper(config =>
-            {
-                config.AddProfile(new MappingProfile(Assembly.GetExecutingAssembly()));
-                config.AddProfile(new MappingProfile(typeof(ITodoListDbContext).Assembly));
-            });
+            services.AddApplication();
+            services.AddInfrastructure(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
