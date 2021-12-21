c6e0f8c29e1f625dae16ee96200c95b51973d1b1
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-21 16:55:07 +0300

    created todo items controller

---
+++ TodoList/TodoList/src/TodoList.Presentation/Controllers/TodoItemController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TodoList.Application.TodoItems.Commands.DeleteTodoItem;
+using TodoList.Application.TodoItems.Commands.UpdateTodoItem;
+using TodoList.Application.TodoItems.Commands.UpdateTodoItemDetails;
+using TodoList.Application.TodoItems.CreateTodoItem.Commands;
+using TodoList.Application.TodoItems.Queries.GetTodoItem;
+
+namespace TodoList.Presentation.Controllers
+{
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [Route("api/{version:apiVersion}/[controller]")]
+    public class TodoItemController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<List<TodoItemDto>> GetTodoItems([FromQuery] GetTodoItemsQuery query)
+        {
+            return await Sender.Send(query);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> CreateTodoItem(CreateTodoItemCommand command)
+        {
+            return await Sender.Send(command);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateTodoItem(Guid id, UpdateTodoItemCommand command)
+        {
+            if(id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await Sender.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult> UpdateTodoItemDetails(Guid id, UpdateTodoItemDetailsCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await Sender.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteTodoItem(Guid id)
+        {
+            await Sender.Send(new DeleteTodoItemCommand { Id = id });
+
+            return NoContent();
+        }
+    }
+}
