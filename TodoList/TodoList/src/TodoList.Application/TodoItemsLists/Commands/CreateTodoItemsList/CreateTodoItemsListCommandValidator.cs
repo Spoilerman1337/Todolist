81f75da53e7f7f26764393edce538d208d4ad2ee
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-22 21:32:29 +0300

    created create todo items list command validator

---
+++ TodoList/TodoList/src/TodoList.Application/TodoItemsLists/Commands/CreateTodoItemsList/CreateTodoItemsListCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using TodoList.Application.Common.Interfaces;
+
+namespace TodoList.Application.TodoItemsLists.Commands.CreateTodoItemsList
+{
+    public class CreateTodoItemsListCommandValidator : AbstractValidator<CreateTodoItemsListCommand>
+    {
+        private readonly ITodoListDbContext _context;
+
+        public CreateTodoItemsListCommandValidator(ITodoListDbContext context)
+        {
+            _context = context;
+
+            RuleFor(v => v.Name)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(50).WithMessage("Title must not exceed 500 characters.")
+                .MustAsync(IsUniqueTitle).WithMessage("Title name must be unique");
+        }
+
+        private async Task<bool> IsUniqueTitle(string name, CancellationToken cancellationToken)
+        {
+            return await _context.TodoLists.AllAsync(v => v.Name != name, cancellationToken);
+        }
+    }
+}
