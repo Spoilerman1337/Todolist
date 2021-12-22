863f1bb7742f69f4c639ffab59a0c950ed6bb5c4
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-22 21:21:46 +0300

    created update todo item details command validator

---
+++ TodoList/TodoList/src/TodoList.Application/TodoItems/Commands/UpdateTodoItemDetails/UpdateTodoItemDetailsCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Application.TodoItems.Commands.UpdateTodoItemDetails
+{
+    public class UpdateTodoItemDetailsCommandValidator : AbstractValidator<UpdateTodoItemDetailsCommand>
+    {
+        public UpdateTodoItemDetailsCommandValidator()
+        {
+            RuleFor(v => v.Id).NotEqual(Guid.Empty);
+            RuleFor(v => v.Note).MaximumLength(500);
+        }
+    }
+}
