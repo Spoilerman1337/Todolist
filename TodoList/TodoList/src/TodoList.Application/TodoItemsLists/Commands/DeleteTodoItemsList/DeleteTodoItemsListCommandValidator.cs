6ccf378210cac016011674894ad3cc72ffc96100
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-22 21:47:52 +0300

    created delete todo items list validator

---
+++ TodoList/TodoList/src/TodoList.Application/TodoItemsLists/Commands/DeleteTodoItemsList/DeleteTodoItemsListCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using System;
+
+namespace TodoList.Application.TodoItemsLists.Commands.DeleteTodoItemsList
+{
+    public class DeleteTodoItemsListCommandValidator : AbstractValidator<DeleteTodoItemsListCommand>
+    {
+        public DeleteTodoItemsListCommandValidator()
+        {
+            RuleFor(v => v.Id).NotEqual(Guid.Empty).WithMessage("ID is required.");
+        }
+    }
+}
