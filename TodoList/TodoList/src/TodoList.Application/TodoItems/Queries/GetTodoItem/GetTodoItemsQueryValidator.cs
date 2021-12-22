453afbbb6dcc7269eb4726ed3482379acc2881ed
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-22 21:24:17 +0300

    created get todo items query validator

---
+++ TodoList/TodoList/src/TodoList.Application/TodoItems/Queries/GetTodoItem/GetTodoItemsQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Application.TodoItems.Queries.GetTodoItem
+{
+    public class GetTodoItemsQueryValidator : AbstractValidator<GetTodoItemsQuery>
+    {
+        public GetTodoItemsQueryValidator()
+        {
+            RuleFor(v => v.ListId).NotEmpty().WithMessage("ListId is required.");
+        }
+    }
+}
