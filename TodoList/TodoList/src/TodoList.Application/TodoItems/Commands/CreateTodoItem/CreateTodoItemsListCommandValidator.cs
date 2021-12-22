8efbf290b974e73d773d00fb2661a12e708441fa
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-22 20:09:10 +0300

    added validation for create item command

---
+++ TodoList/TodoList/src/TodoList.Application/TodoItems/Commands/CreateTodoItem/CreateTodoItemsListCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using TodoList.Application.TodoItems.CreateTodoItem.Commands;
+
+namespace TodoList.Application.TodoItems.Commands.CreateTodoItem
+{
+    public class CreateTodoItemsListCommandValidator : AbstractValidator<CreateTodoItemCommand>
+    {
+        public CreateTodoItemsListCommandValidator()
+        {
+            RuleFor(v => v.Title).NotEmpty().MaximumLength(50);
+            RuleFor(v => v.ListId).NotEqual(Guid.Empty);
+        }
+    }
+}
