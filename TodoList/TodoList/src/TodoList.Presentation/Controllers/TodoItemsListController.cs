927e62f67c6e6ef9ef79cf02ae8cafab39f283ba
Maxim Toropygin <mrcatman1337@gmail.com>
2021-12-27 20:11:14 +0300

    added swagger xml documentation for todo list controller

--- TodoList/TodoList/src/TodoList.Presentation/Controllers/TodoItemsListController.cs
+++ TodoList/TodoList/src/TodoList.Presentation/Controllers/TodoItemsListController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
 using TodoList.Application.TodoItemsLists.Commands.CreateTodoItemsList;
@@ -12,19 +13,59 @@ namespace TodoList.Presentation.Controllers
     [Route("api/[controller]")]
     public class TodoItemsListController : ApiControllerBase
     {
+        /// <summary>
+        /// Gets the todo lists
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// GET /todoitemslist/
+        /// </remarks>
+        /// <returns>Returns TodoItemsListVm</returns>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<TodoItemsListVm>> GetTodoList()
         {
             return await Sender.Send(new GetTodoItemsListQuery());
         }
 
+        /// <summary>
+        /// Creates the todo list
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// POST /todoitem/
+        /// </remarks>
+        /// <param name="command">CreateTodoItemsListCommand object</param>
+        /// <returns>Returns newly created list id (GUID)</returns>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<Guid>> CreateTodoList(CreateTodoItemsListCommand command)
         {
             return await Sender.Send(command);
         }
 
+        /// <summary>
+        /// Updates the todo list
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// PUT /todoitem/072f52e5-6e70-4376-9214-c3af8a7b55cb
+        /// </remarks>
+        /// <param name="id">Todo list id (GUID)</param>
+        /// <param name="command">UpdateTodoItemsListCommand object</param>
+        /// <response code="204">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="400">Bad Request</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult> UpdateTodoList(Guid id, UpdateTodoItemsListCommand command)
         {
             if(id != command.Id)
@@ -37,7 +78,19 @@ namespace TodoList.Presentation.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Deletes todo list
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// DELETE /todoitem/072f52e5-6e70-4376-9214-c3af8a7b55cb
+        /// </remarks>
+        /// <param name="id">Todo list id (GUID)</param>
+        /// <response code="204">Success</response>
+        /// <response code="401">Unauthorized</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult> DeleteTodoList(Guid id)
         {
             await Sender.Send(new DeleteTodoItemsListCommand { Id = id });
